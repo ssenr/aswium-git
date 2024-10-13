dbaf305f3dfd5a7c876c13a1381d038c1efa8f79
Vallis-LFC <vkh.luong@gmail.com>
2024-10-12 22:48:34 -0400

    server side done

--- src/main.c
+++ src/main.c
@@ -44,6 +44,23 @@ int main(int argc, char const* argv[])
         INADDR_ANY to specify IP address
     */
     if(bind(serverfd,(struct sockaddr* )&address,sizeof(address))<0){
-
+        perror("bind failed");
+        exit(EXIT_FAILURE);
     }
+    if(listen(serverfd,3)<0){
+        perror("listen function failed");
+        exit(EXIT_FAILURE);
+    }
+    if(new_socket = accept(serverfd,(struct sockaddr*)&address,&addrlength)){
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read(new_socket,buffer,1024 -1); //1 is subtracted for the null terminator
+    printf("%s\n",buffer);
+    send(new_socket, msg, strlen(msg),0);
+    printf("msg sent\n");
+    close(new_socket);
+    close(serverfd);
+    return 0;
+
 }
