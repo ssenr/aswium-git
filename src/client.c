3d4d640fbb104034ad705a2e508684a77355d16d
Vallis-LFC <vkh.luong@gmail.com>
2024-10-12 23:29:19 -0400

    client

--- src/client.c
+++ src/client.c
@@ -17,5 +17,19 @@ int main(int argc, char const* argv[]){
     serv_addr.sin_port = htons(8080);
 
     //ip addr from txt to bin form
-    if(inet_pton(AF_INET,"12"))
+    if(inet_pton(AF_INET,"127.0.0.1",&serv_addr.sin_addr)<=0){
+        printf("\n Invalid address/ address not supported \n");
+        return -1;
+    }
+    if((status = connect(clientfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr)))<0){
+        printf("\nConnection failed \n");
+        return -1;
+    }
+    send(clientfd,msg,strlen(msg),0);
+    return -1;
+    printf("msg from client side sent\n");
+    valread = read(clientfd,buffer,1024 -1);
+    printf("%s\n", buffer);
+    close(clientfd);
+    return 0;
 }
