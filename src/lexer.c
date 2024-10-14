95bae5f36ca5bf63e95657000e0cd0c7bfa80b21
xhrithik <hrithik@sarkar.gg>
2024-10-14 03:08:45 -0400

    did wittle bit of work

--- src/lexer.c
+++ src/lexer.c
@@ -1,4 +1,9 @@
-void lexer(void)
-{
-	// It's Lexing Time
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "lexer.h"
+
+static void skip_whitespace(const char *input, int *pos) {
+    while (isspace(input[*pos])) (*pos)++;
 }
--- src/lexer.h
+++ src/lexer.h
@@ -1,6 +1,26 @@
 #ifndef lexer_h
 #define lexer_h
 
-// Define Functions
+typedef enum {
+  TOKEN_HEADING,
+  TOKEN_BOLD,
+  TOKEN_ITALIC,
+  TOKEN_BLOCKQUOTE,
+  TOKEN_ORDEREDLIST,
+  TOKEN_UNORDEREDLIST,
+  TOKEN_CODE,
+  TOKEN_HORIZONTALRULE,
+  TOKEN_LINK,
+  TOKEN_IMAGE,
+  TOKEN_FENCECODEBLOCK,
+  TOKEN_FOOTNOTE,
+  TOKEN_SUBSCRIPT,
+  TOKEN_SUPERSCRIPT,
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char *value; //this is a pointer heh just learned this
+} Token;
 
 #endif
