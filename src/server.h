9b77c8f78515e77cb5f400a8999fd56318d50742
Vallis-LFC <vkh.luong@gmail.com>
2024-10-16 23:15:18 -0400

    objectives in server.h if u didnt put em in readme yet

--- src/server.h
+++ src/server.h
@@ -3,3 +3,15 @@
 void run();
 
 //tester header info, read out html
+//design a system of tests
+//printing the http header, content of the tcp packet
+//get a history of git commits, history, pulls,
+//authentication, if pushing at this place, with this cred ok, if not get out
+//site generator, to generate all the code and shi
+//static code analyzers, one that reads the code and analyze and hightlight it
+//read the name of the file and hight light it ina  certain way, percentages of different kinds of code
+//
+char *data = strstr( buffer, "\r\n\r\n")
+while (recv(sockfd, buffer, 1, 0)){
+    
+}
