37e9ae012b58d61e0777d4ac590c607410dac373
ssenr <duranr@protonmail.com>
2024-12-18 21:13:11 -0500

    Routing Not Working Yet :(

--- src/server.c
+++ src/server.c
@@ -10,6 +10,7 @@
 #include <netinet/in.h>
 
 #define BUFFER_SIZE     1048576
+#define PORT            8080
 
 // Strangely similar to code from Jeffery Yu, found at:
 // https://dev.to/jeffreythecoder/how-i-built-a-simple-http-server-from-scratch-using-c-739
@@ -47,6 +48,78 @@ char* get_file_name(const char* fn)
     return ext;
 }
 
+char* get_mime_type(const char* file_ext)
+{
+    // Check file extension of requested file
+    // Return corresponding mime-type
+    // mime-types supported: .css .html .ico .png .jpg
+    if(strcmp(file_ext, "html") == 0)
+    {
+        return "text/html";
+    }
+    else if (strcmp(file_ext, "jpg") == 0)
+    {
+        return "image/jpeg";
+    }
+    else if (strcmp(file_ext, "jpeg") == 0)
+    {
+        return "image/jpeg";
+    }
+    else if (strcmp(file_ext, ".ico") == 0)
+    {
+        return "image/x-icon";
+    }
+    else if (strcmp(file_ext, ".css") == 0)
+    {
+        return "text/css";
+    }
+    return "";
+}
+
+char* extract_first_line(const char* header)
+{
+    // TODO: Implement Edge Case
+    // Edge Case: If string doesn't have a \n character
+    const char* newline_pos = strchr(header, '\n');
+    size_t len = newline_pos - header;
+
+    char* first_line = (char*)malloc(len+1);
+    strncpy(first_line, header, len);
+    first_line[len] = '\0';
+    return first_line;
+}
+
+char* get_requested_resource_path(const char* get_request)
+{
+    const char* pattern = "GET (.+) HTTP/1\\.1";
+    regex_t regex;
+    regmatch_t matches[2];
+
+    regcomp(&regex, pattern, REG_EXTENDED);
+
+    regexec(&regex, get_request, 2 ,matches, 0);
+
+    size_t len = matches[1].rm_eo - matches[1].rm_so;
+
+    char* buf = (char*)malloc(len+1);
+
+    strncpy(buf, get_request + matches[1].rm_so, len);
+
+    buf[len] = '\0';
+
+    regfree(&regex);
+
+    size_t new_len = strlen(buf);
+    char* new_buf =(char*)malloc(new_len);
+
+    new_buf[0] = '.';
+    new_buf[1] = '\0';
+
+    strncat(new_buf, buf, new_len);
+
+    return new_buf;
+}
+
 char* http_header_ok() // index.html
 {
   char* http_header = "HTTP/1.1 200 OK\r\n\n";
@@ -62,6 +135,51 @@ char* http_header_ok() // index.html
   return str;
 }
 
+char* get_file_data(char* path)
+{
+    // READ FILE
+    FILE* file = fopen(path, "rb");
+    fseek(file, -1, SEEK_END);
+    long file_size = ftell(file);
+    rewind(file);
+
+    char* file_data = malloc(file_size);
+    fread(file_data, 0, file_size, file);
+    fclose(file);
+
+    return file_data;
+}
+
+long get_file_size(char* path)
+{
+    // READ FILE
+    FILE *file = fopen(path, "rb");
+    fseek(file, -1, SEEK_END);
+    long file_size = ftell(file);
+    rewind(file);
+
+    char *file_data = malloc(file_size);
+    fread(file_data, 0, file_size, file);
+    fclose(file);
+
+    return file_size;
+}
+
+char* build_header(const char* mime_type, long file_size)
+{
+    char* header = malloc(BUFFER_SIZE);
+    snprintf(header, BUFFER_SIZE,
+        "HTTP/0.1 200 OK \r\n"
+        "Content-Type: %s\r\n"
+        "Content-Length: %ld\r\n"
+        "Connection: close\r\n"
+        "\r\n",
+        mime_type,
+        file_size
+    );
+    return header;
+}
+
 void* handle_client(void* fd)
 {
     int client_fd = *((int*) fd);
@@ -70,89 +188,6 @@ void* handle_client(void* fd)
     char* buffer = (char*)malloc(BUFFER_SIZE);
 
     ssize_t bytes_recieved = recv(client_fd, buffer, BUFFER_SIZE, 0);
-    // printf("Recieved request:\n%s\n", buffer);
-
-    // CSS
-    // if (strncmp(buffer, "GET /src/html/index.css HTTP/1.1", 24) == 0)
-    // {
-    //     FILE* file = fopen("./src/html/index.css", "rb");
-    //     fseek(file, 0, SEEK_END);
-    //     long file_size = ftell(file);
-    //     rewind(file);
-
-    //     char* file_data = malloc(file_size);
-    //     fread(file_data, 1, file_size, file);
-    //     fclose(file);
-
-    //     char* file_extension = get_file_ext(get_file_name("./src/html/index.css"));
-    //     char response_header[BUFFER_SIZE];
-    //     snprintf(response_header, BUFFER_SIZE,
-    //              "HTTP/1.1 200 OK\r\n"
-    //              "Content-Type: text/%s\r\n"
-    //              "Content-Length: %ld\r\n"
-    //              "Connection: close\r\n"
-    //              "\r\n",
-    //              file_extension,
-    //              file_size);
-
-    //     send(client_fd, response_header, strlen(response_header), 0);
-    //     send(client_fd, file_data, file_size, 0);
-    //     free(file_data);
-    // }
-
-    // // FAVICON LOL
-    // if (strncmp(buffer, "GET /favicon.ico HTTP/1.1", 24) == 0)
-    // {
-    //     FILE* file = fopen("./src/resources/favicon.ico", "rb");
-    //     fseek(file, 0, SEEK_END);
-    //     long file_size = ftell(file);
-    //     rewind(file);
-
-    //     char* file_data = malloc(file_size);
-    //     fread(file_data, 1, file_size, file);
-    //     fclose(file);
-
-
-    //     char response_header[BUFFER_SIZE];
-    //     snprintf(response_header, BUFFER_SIZE,
-    //              "HTTP/1.1 200 OK\r\n"
-    //              "Content-Type: image/x-icon\r\n"
-    //              "Content-Length: %ld\r\n"
-    //              "Connection: close\r\n"
-    //              "\r\n",
-    //              file_size);
-
-    //     send(client_fd, response_header, strlen(response_header), 0);
-    //     send(client_fd, file_data, file_size, 0);
-    //     free(file_data);
-    // }
-
-    // // Spongebob
-    // if (strncmp(buffer, "GET /src/resources/spongebob.png HTTP/1.1", 24) == 0)
-    // {
-    //     FILE* file = fopen("./src/resources/spongebob.png", "rb");
-    //     fseek(file, 0, SEEK_END);
-    //     long file_size = ftell(file);
-    //     rewind(file);
-
-    //     char* file_data = malloc(file_size);
-    //     fread(file_data, 1, file_size, file);
-    //     fclose(file);
-
-    //     char response_header[BUFFER_SIZE];
-
-    //     snprintf(response_header, BUFFER_SIZE,
-    //              "HTTP/1.1 200 OK\r\n"
-    //              "Content-Type: image/png\r\n"
-    //              "Content-Length: %ld\r\n"
-    //              "Connection: close\r\n"
-    //              "\r\n",
-    //              file_size);
-
-    //     send(client_fd, response_header, strlen(response_header), 0);
-    //     send(client_fd, file_data, file_size, 0);
-    //     free(file_data);
-    // }
 
     /**
      *  Extract URL from GET Request (Basic Routing)
@@ -160,6 +195,7 @@ void* handle_client(void* fd)
      *  If it does, return it
      *  else, return 404
      */
+
     if (bytes_recieved > 0)
     {
         regex_t regex;
@@ -169,14 +205,26 @@ void* handle_client(void* fd)
         
         if (regexec(&regex, buffer, 2, matches, 0) == 0)
         {
-            printf("Extracted REGEX:\n%s", buffer + buffer[matches[1].rm_eo]);
-            printf("Extracted REGEX:\n%s", buffer + buffer[matches[1].rm_so]);
-            printf("Another Extracted REGEX: \n%s", buffer + buffer[matches[2].rm_eo]);
-            printf("Another Extracted REGEX: \n%s", buffer + buffer[matches[2].rm_so]);
-            char* header = http_header_ok();
-            int len = strlen(header);
-            send(client_fd, header, len, 0);
-            free(header);
+            // Get file path of requested resources 
+            char* file_path = get_requested_resource_path(buffer);
+            char* file_ext = get_file_ext(file_path);
+            char* mime_type = get_mime_type(file_ext);
+
+            char* file_data = get_file_data(file_path);
+            long file_size = get_file_size(file_path);
+
+            char* header1 = build_header(mime_type, file_size);
+
+            // char* header = http_header_ok();
+            int len = strlen(header1);
+            send(client_fd, header1, len, 0);
+            send(client_fd, file_data, file_size, -1);
+
+            free(header1);
+            free(file_path);
+            free(file_ext);
+            free(mime_type);
+            free(file_data);
         }
         regfree(&regex);
     }
@@ -189,6 +237,16 @@ void* handle_client(void* fd)
 void run_server()
 {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // SET SOCKET OPTIONS
+    int opt = 1;
+    int sock_opt_status = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    if (sock_opt_status)
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
     struct sockaddr_in server_addr;
     if ( server_fd < 0)
     {
@@ -198,7 +256,7 @@ void run_server()
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(8081);
+    server_addr.sin_port = htons(PORT);
 
     int bind_status = bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr));
 
--- src/server.h
+++ src/server.h
@@ -1,3 +1,4 @@
 #pragma once
 
 void run_server();
+char* get_requested_resource_path(const char* get_request);
